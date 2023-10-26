2b02fbccc56f255ddd84c5ab9f9ec8c103e4a08f
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-10-27 06:44:16 +0700

    create middleware to validate post request

---
+++ middleware/middleware.js
@@ -0,0 +1,27 @@
+const { ResponseTemplate } = require('../helper/response.helper');
+const Joi = require('joi');
+
+function CheckPostReq(req, res, next) {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().max(50).required(),
+    email: Joi.string().alphanum().email().required(),
+    password: Joi.string().alphanum().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    let respErr = ResponseTemplate(
+      null,
+      'invalid request',
+      error.details[0].message,
+      400
+    );
+    res.json(respErr);
+    return;
+  }
+
+  next();
+}
+
+module.exports = CheckPostReq;
