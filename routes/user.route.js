1e76e3e301b53bce8d511d25fe0cafbd51382456
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-06 13:49:33 +0700

    [CREATE] user POST data API documentation

--- routes/user.route.js
+++ routes/user.route.js
@@ -11,6 +11,69 @@ const {
   ValidateUpdateUserRequest,
 } = require('../middleware/middleware');
 
+/**
+ * @openapi
+ * /users:
+ *  post:
+ *    tags:
+ *      - Users
+ *    responses:
+ *      201:
+ *        description: created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: integer
+ *                  example: 201
+ *                message:
+ *                  type: string
+ *                  example: success
+ *                error:
+ *                  type: object
+ *                  example: null
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                      example: 1
+ *                    name:
+ *                      type: string
+ *                      example: Retno
+ *                    email:
+ *                      type: string
+ *                      example: retno@gmail.com
+ *                    createdAt:
+ *                      type: string
+ *                      example: 2023-11-06T06:32:31.301Z
+ *                    updatedAt:
+ *                      type: string
+ *                      example: 2023-11-06T06:32:31.301Z
+ *                    profile:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: integer
+ *                          example: 1
+ *                        identityType:
+ *                          type: string
+ *                          example: KTP
+ *                        identityNumber:
+ *                          type: string
+ *                          example: 32750411
+ *                        address:
+ *                          type: string
+ *                          example: Jl. Sukaria
+ *                        createdAt:
+ *                          type: string
+ *                          example: 2023-11-06T06:32:31.301Z
+ *                        updatedAt:
+ *                          type: string
+ *                          example: 2023-11-06T06:32:31.301Z
+ */
 userRouter.post('/', ValidateCreateUserRequest, Insert);
 
 /**
