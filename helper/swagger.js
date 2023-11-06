ec8b2dabf65dd8903063902f1de01c1fe0e0b871
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-06 10:17:31 +0700

    [MODIFIED] route swagger

--- helper/swagger.js
+++ helper/swagger.js
@@ -19,7 +19,7 @@ const swaggerSpec = swaggerJSDoc(options);
 // Function to setup our docs
 const swaggerDocs = (app, port) => {
   // Route-Handler to visit our docs
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   // Make our docs in JSON format available
   app.get('/api/v1/docs.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
