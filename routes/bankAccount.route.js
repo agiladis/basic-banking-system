072930071c5ae3078c3c2341575ed03662da7859
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-07 03:54:33 +0700

    [CREATE] component schemas BankAccount routes

--- routes/bankAccount.route.js
+++ routes/bankAccount.route.js
@@ -9,6 +9,47 @@ const {
   ValidateCreateBankAccountRequest,
 } = require('../middleware/middleware');
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     BankRequest:
+ *       type: object
+ *       properties:
+ *         userId:
+ *           type: integer
+ *           example: 5
+ *         bankName:
+ *           type: string
+ *           example: GoPay
+ *         balance:
+ *           type: integer
+ *           example: 100000
+ *
+ *     BankResponse:
+ *       allOf:
+ *         - type: object
+ *           properties:
+ *             id:
+ *               type: integer
+ *               example: 5
+ *            bankAccountNumber:
+ *              type: string
+ *              example: 1699303624686
+ *         - $ref: '#/components/schemas/BankRequest'
+ *         - $ref: '#/components/schemas/LastActionResponse'
+ *
+ *     LastActionResponse:
+ *       type: object
+ *       properties:
+ *         createdAt:
+ *           type: string
+ *           example: 2023-11-06T06:32:31.301Z
+ *         updatedAt:
+ *           type: string
+ *           example: 2023-11-06T06:32:31.301Z
+ */
+
 bankAccountRouter.post('/', ValidateCreateBankAccountRequest, Insert);
 bankAccountRouter.get('/', GetAll);
 bankAccountRouter.get('/:id', GetById);
