a0a3c903a25c4561ce65f8ccc0c7c92308d98cb2
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-11-07 16:04:23 +0700

    [CREATE] Transaction POST data API documentation

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -23,12 +23,9 @@ const {
  *           type: integer
  *           example: 1699303624686
  *
- *     Transaction:
+ *     TransactionRequest:
  *       type: object
  *       properties:
- *         id:
- *           type: integer
- *           example: 1
  *         amount:
  *           type: integer
  *           example: 2000
@@ -38,9 +35,6 @@ const {
  *         destinationAccountId:
  *           type: integer
  *           example: 1
- *         createdAt:
- *           type: string
- *           example: 2023-11-06T06:32:31.301Z
  *
  *     LastActionResponse:
  *       type: object
@@ -53,6 +47,51 @@ const {
  *           example: 2023-11-06T06:32:31.301Z
  */
 
+/**
+ * @openapi
+ * /transactions:
+ *  post:
+ *    summary: Create new Interbank Transaction
+ *    tags:
+ *      - Transactions
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            allOf:
+ *              - $ref: '#/components/schemas/TransactionRequest'
+ *    responses:
+ *      201:
+ *        description: created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: integer
+ *                  example: 201
+ *                message:
+ *                  type: string
+ *                  example: success
+ *                error:
+ *                  type: object
+ *                  example: null
+ *                data:
+ *                  allOf:
+ *                    - type: object
+ *                      properties:
+ *                        id:
+ *                          type: integer
+ *                          example: 1
+ *                    - $ref: '#/components/schemas/TransactionRequest'
+ *                    - type: object
+ *                      properties:
+ *                        createdAt:
+ *                          type: string
+ *                          example: 2023-11-06T06:32:31.301Z
+ */
 transactionRoute.post('/', ValidateCreateTransactionRequest, Insert);
 transactionRoute.get('/', GetAll);
 transactionRoute.get('/:id', GetById);
